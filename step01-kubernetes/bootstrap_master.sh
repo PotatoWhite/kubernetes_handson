bac810759db507f059f3924934285f2e84731a9a
PotatoWhite <bravopotato@gmail.com>
2021-04-21 01:20:08 +0900

    fix bug and add some guide

---
+++ step01-kubernetes/bootstrap_master.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "[TASK 1] Pull required containers"
+kubeadm config images pull >/dev/null 2>&1
+
+echo "[TASK 2] Initialize Kubernetes Cluster"
+kubeadm init --apiserver-advertise-address=172.16.0.10 --pod-network-cidr=10.244.0.0/16 >> /root/kubeinit.log 2>/dev/null
+
+echo "[TASK 3] Deploy Calico network"
+kubectl --kubeconfig=/etc/kubernetes/admin.conf create -f https://docs.projectcalico.org/v3.16/manifests/calico.yaml >/dev/null 2>&1
+
+echo "[TASK 4] Generate and save cluster join command to /joincluster.sh"
+kubeadm token create --print-join-command > /joincluster.sh 2>/dev/null
+
+echo "[TASK 5] Make kube config for vagrant"
+mkdir /home/vagrant/.kube
+cp -R /etc/kubernetes/admin.conf /home/vagrant/.kube/config
+chown vagrant:vagrant /home/vagrant/.kube
+chown vagrant:vagrant /home/vagrant/.kube/config
